f850ff7e244ca99c67056db9fd0642c57bb821fa
Space Monkey <siguelasreglas@gmail.com>
2015-03-31 11:26:15 +0200

    PruebaGit
    Nuevo

---
+++ Pruebas/src/pruebas/Suma.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author InTylerWeTrust
+ */
+public class Suma {
+
+ int n1 = 3;
+ int n2 = 3;
+ int n3 = 3;
+         
+ 
+ 
+}
