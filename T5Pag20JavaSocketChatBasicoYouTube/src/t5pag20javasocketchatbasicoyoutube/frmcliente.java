9cc89f211caaec0ec4bbac443b54213e53579388
Space Monkey <siguelasreglas@gmail.com>
2015-04-04 21:01:45 +0200

    T5Pag20JavaSocketChatBasico
    Ejemplo del youtube
    https://www.youtube.com/watch?v=I1XdWUjpXpM

---
+++ T5Pag20JavaSocketChatBasicoYouTube/src/t5pag20javasocketchatbasicoyoutube/frmcliente.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t5pag20javasocketchatbasicoyoutube;
+
+import java.awt.HeadlessException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author InTylerWeTrust
+ */
+public class frmcliente extends JFrame {
+
+//caja de texto 
+ JTextField txtNick; 
+//caja para poner la ip a la que vas a mandar el mensaje
+ JTextField txtIP;  
+ JTextField txtMensaje;
+ 
+//caja de boton 
+ JButton btnEnviar;
+ 
+         
+ 
+// 
+ public frmcliente() throws HeadlessException {
+ }
+ 
+ 
+ 
+}
